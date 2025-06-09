bb06310afd5dcbded857c2013cf14a741ca5d70e
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-06-09 11:21:12 -0500

    Save a plot example

--- main.py
+++ main.py
@@ -24,4 +24,19 @@ county_seasonal_avg = seasonal_totals.groupby(['YEAR', 'SEASON'])['PRCP'].mean()
 county_seasonal_avg = county_seasonal_avg[county_seasonal_avg['YEAR'] < 2025]
 
 
+
+#os.makedirs("images", exist_ok=True) # But your directory is Images...
+
+pivot_table_avg = county_seasonal_avg.pivot(index='YEAR', columns='SEASON', values='PRCP').fillna(0)
+pivot_table_avg = pivot_table_avg[['Winter', 'Spring', 'Summer', 'Fall']]  # Order seasons
+
+pivot_table_avg.plot(kind='bar', stacked=True, figsize=(12, 6), colormap='Set2')
+plt.title("Average Seasonal Rainfall in Waukesha County (All Stations, Excluding 2025)")
+plt.xlabel("Year")
+plt.ylabel("Avg. Precipitation per Station (inches)")
+plt.legend(title="Season") # Should be able to relocate the legend here too
+plt.tight_layout()
+
+plt.savefig("Images/AHHHHHHHHH.png", dpi=300)  # Save the figure with high resolution
+
 # %%
